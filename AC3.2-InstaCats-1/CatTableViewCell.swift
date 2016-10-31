02e4f809a5ed4e7f42391955f98284ce57ca5ef8
defintuition <ahspadafora@gmail.com>
2016-10-31 00:28:11 -0400

    hw

---
+++ AC3.2-InstaCats-1/CatTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CatTableViewCell.swift
+//  AC3.2-InstaCats-1
+//
+//  Created by Amber Spadafora on 10/30/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class CatTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
